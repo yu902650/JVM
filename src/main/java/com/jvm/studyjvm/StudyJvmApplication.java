c579446eaeb64af33576481b6d099d74fef354a9
BoYu <974619916@qq.com>
2019-07-04 13:55:53 +0800

    first commit code

---
+++ src/main/java/com/jvm/studyjvm/StudyJvmApplication.java
@@ -0,0 +1,13 @@
+package com.jvm.studyjvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJvmApplication.class, args);
+    }
+
+}
